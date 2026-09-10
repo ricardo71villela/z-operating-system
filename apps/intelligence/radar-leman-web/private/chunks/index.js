091a6bfc608919599759cf34131d309791456258
Ricardo Vilela <ricardo.villela@portinvestco.com>
2026-09-10 22:34:31 +0200

    Radar Immobilier: dashboard atualizado (18 671 moradas, seuil >= 40)

--- apps/intelligence/radar-leman-web/private/chunks/index.js
+++ apps/intelligence/radar-leman-web/private/chunks/index.js
@@ -4,4 +4,9 @@ module.exports = [
   require('./chunk-1.js'),
   require('./chunk-2.js'),
   require('./chunk-3.js'),
+  require('./chunk-4.js'),
+  require('./chunk-5.js'),
+  require('./chunk-6.js'),
+  require('./chunk-7.js'),
+  require('./chunk-8.js'),
 ];
